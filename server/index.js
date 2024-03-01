14bd66a47ab33ac3f40669c2edba53757b2e3e42
Amandeep Singh <programmeramandeep@gmail.com>
2024-03-01 17:50:30 +0530

    Setup passport google auth strategy

--- server/index.js
+++ server/index.js
@@ -1,15 +1,26 @@
-const express = require("express");
-const cors = require("cors");
-const config = require("./utils/config");
+const express = require('express');
+const cors = require('cors');
+const passport = require('passport');
+const GoogleStrategy = require('passport-google-oauth20');
+const config = require('./utils/config');
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
 
-app.get("/", (req, res) => {
-  res.send("Hello world");
-});
+passport.use(
+  new GoogleStrategy(
+    {
+      clientId: config.GOOGLE_CLIENT_ID,
+      clientSecret: config.GOOGLE_CLIENT_SECRET,
+      callbackURL: '/auth/google/callback',
+    },
+    (accessToken) => {
+      console.log(accessToken);
+    }
+  )
+);
 
 app.listen(config.SERVER_PORT, () => {
   console.log(`Server is running on http://localhost:${config.SERVER_PORT}`);
--- server/utils/config.js
+++ server/utils/config.js
@@ -1,11 +1,15 @@
-require("dotenv").config();
+require('dotenv').config();
 
 const MONGO_URI = process.env.MONGO_URI;
 const NODE_ENV = process.env.NODE_ENV;
 const SERVER_PORT = process.env.SERVER_PORT;
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_OAUTH_CLIENT_ID;
+const GOOGLE_CLIENT_SECRET = process.env.GOOGLE_OAUTH_CLIENT_SECRET;
 
 module.exports = {
   MONGO_URI,
   NODE_ENV,
   SERVER_PORT,
+  GOOGLE_CLIENT_ID,
+  GOOGLE_CLIENT_SECRET,
 };
