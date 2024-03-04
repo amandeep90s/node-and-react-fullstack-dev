6b69d6aef5cea208b6c8e2fc07a7f56f08612e25
Amandeep Singh <programmeramandeep@gmail.com>
2024-03-04 21:23:22 +0530

    Cleaned client setup

--- client/vite.config.js
+++ client/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react-swc';
+import dotenv from 'dotenv';
+
+dotenv.config(); // Load environment from .env file
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    proxy: {
+      '/api': {
+        target: process.env.VITE_APP_SERVER_URL,
+        changeOrigin: true,
+      },
+    },
+  },
+});
