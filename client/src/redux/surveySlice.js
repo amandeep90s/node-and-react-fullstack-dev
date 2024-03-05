d5d5b3873e18efe6462f1f5f770f746d14f9db30
Amandeep Singh <programmeramandeep@gmail.com>
2024-03-05 19:55:56 +0530

    Setup redux toolkit

---
+++ client/src/redux/surveySlice.js
@@ -0,0 +1,9 @@
+import { createSlice } from '@reduxjs/toolkit';
+
+const surveySlice = createSlice({
+  name: 'survey',
+  initialState: {},
+  reducers: {},
+});
+
+export default surveySlice.reducer;
